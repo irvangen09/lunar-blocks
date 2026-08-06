52068685fae605d43f10a4ed068d73cd65bde97f
nymphadora07 <cocnymphadora@gmail.com>
2026-08-06 14:16:39 +0700

    feat(definition-list): add parent static markup

---
+++ src/definition-list/save.js
@@ -0,0 +1,19 @@
+import { useBlockProps, RichText } from '@wordpress/block-editor';
+
+export default function save( { attributes } ) {
+	const { variant, content } = attributes;
+
+	const blockProps = useBlockProps.save( {
+		className: `lunar-callout lunar-callout--${ variant }`,
+	} );
+
+	return (
+		<div { ...blockProps }>
+			<RichText.Content
+				tagName="div"
+				className="lunar-callout__text"
+				value={ content }
+			/>
+		</div>
+	);
+}
