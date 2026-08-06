47c2e3e156836c474cef18489d76f3245f11eceb
nymphadora07 <cocnymphadora@gmail.com>
2026-08-06 14:15:36 +0700

    feat(definition-list): add parent editor wrapping InnerBlocks

---
+++ src/definition-list/edit.js
@@ -0,0 +1,23 @@
+import { useBlockProps, useInnerBlocksProps, InnerBlocks } from '@wordpress/block-editor';
+
+const ALLOWED_BLOCKS = [ 'lunar-blocks/definition-item' ];
+
+const TEMPLATE = [
+	[ 'lunar-blocks/definition-item' ],
+	[ 'lunar-blocks/definition-item' ],
+];
+
+export default function Edit() {
+	const blockProps = useBlockProps( {
+		className: 'lunar-definition-list',
+	} );
+
+	const innerBlocksProps = useInnerBlocksProps( blockProps, {
+		allowedBlocks: ALLOWED_BLOCKS,
+		template: TEMPLATE,
+		templateLock: false,
+		renderAppender: InnerBlocks.ButtonBlockAppender,
+	} );
+
+	return <dl { ...innerBlocksProps } />;
+}
