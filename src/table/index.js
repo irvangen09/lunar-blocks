c98ad5bfe0f80db5e9b2e16cf214fa0365ffca51
nymphadora07 <cocnymphadora@gmail.com>
2026-08-10 10:01:22 +0700

    feat(table): register block entry point

---
+++ src/table/index.js
@@ -0,0 +1,14 @@
+import { registerBlockType } from '@wordpress/blocks';
+
+import metadata from './block.json';
+import Edit from './edit';
+import save from './save';
+
+import './style.scss';
+import './editor.scss';
+
+registerBlockType( metadata.name, {
+	icon: 'editor-table',
+	edit: Edit,
+	save,
+} );
