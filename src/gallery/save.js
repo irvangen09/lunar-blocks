f86f339bd8ef430ce4f7e6000609aa38e2fd2a36
nymphadora07 <cocnymphadora@gmail.com>
2026-08-10 23:17:40 +0700

    feat(gallery): add Gallery parent static markup

---
+++ src/gallery/save.js
@@ -0,0 +1,13 @@
+import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
+
+export default function save( { attributes } ) {
+	const { columns } = attributes;
+
+	const blockProps = useBlockProps.save( {
+		className: `lunar-gallery lunar-gallery--columns-${ columns }`,
+	} );
+
+	const innerBlocksProps = useInnerBlocksProps.save( blockProps );
+
+	return <div { ...innerBlocksProps } />;
+}
