d7568260e68978cfe6b30773b17efd8212fc3af1
nymphadora07 <cocnymphadora@gmail.com>
2026-08-11 16:50:40 +0700

    feat(timeline): add child block static markup

---
+++ src/timeline/item/save.js
@@ -0,0 +1,36 @@
+import { useBlockProps, RichText } from '@wordpress/block-editor';
+
+export default function save( { attributes } ) {
+	const { label, title, description } = attributes;
+
+	const blockProps = useBlockProps.save( {
+		className: 'lunar-timeline-item',
+	} );
+
+	const hasLabel = ! RichText.isEmpty( label );
+	const hasDescription = ! RichText.isEmpty( description );
+
+	return (
+		<li { ...blockProps }>
+			{ hasLabel && (
+				<RichText.Content
+					tagName="span"
+					className="lunar-timeline-item__label"
+					value={ label }
+				/>
+			) }
+			<RichText.Content
+				tagName="h3"
+				className="lunar-timeline-item__title"
+				value={ title }
+			/>
+			{ hasDescription && (
+				<RichText.Content
+					tagName="div"
+					className="lunar-timeline-item__description"
+					value={ description }
+				/>
+			) }
+		</li>
+	);
+}
