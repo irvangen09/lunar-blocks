86bed91006bb18e8067f1c299fc5896c01807114
nymphadora07 <cocnymphadora@gmail.com>
2026-08-11 12:12:02 +0700

    feat(timeline): add parent block static markup

---
+++ src/timeline/save.js
@@ -0,0 +1,11 @@
+import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
+
+export default function save() {
+	const blockProps = useBlockProps.save( {
+		className: 'lunar-timeline',
+	} );
+
+	const innerBlocksProps = useInnerBlocksProps.save( blockProps );
+
+	return <ol { ...innerBlocksProps } />;
+}
