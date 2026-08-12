93ec2b01031b936001a28901809f0c95f7eae5e9
nymphadora07 <cocnymphadora@gmail.com>
2026-08-12 15:16:45 +0700

    feat(infobox): wire up Infobox_Sync and bump to 1.4.0

--- lunar-blocks.php
+++ lunar-blocks.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Lunar Blocks
  * Plugin URI:        https://github.com/irvangen09/lunar-blocks
  * Description:       Standalone Gutenberg block collection for documentation-style content. Works on any WordPress site, with optional enhancements when Lunar Wiki is active.
- * Version:           1.3.0
+ * Version:           1.4.0
  * Requires at least: 6.5
  * Requires PHP:      8.0
  * Author:            Irvan Noerfazri
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'LUNAR_BLOCKS_VERSION', '1.3.0' );
+define( 'LUNAR_BLOCKS_VERSION', '1.4.0' );
 define( 'LUNAR_BLOCKS_PLUGIN_FILE', __FILE__ );
 define( 'LUNAR_BLOCKS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'LUNAR_BLOCKS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
@@ -96,5 +96,8 @@ function bootstrap(): void {
 
 	$heading_injector = new Heading_Injector();
 	$heading_injector->init();
+
+	$infobox_sync = new Infobox_Sync();
+	$infobox_sync->init();
 }
 add_action( 'plugins_loaded', __NAMESPACE__ . '\\bootstrap' );
